ba79a00ad7ba4178f0e84ffa0678e2258e14a0f0
狐白 <1831605011@qq.com>
2021-05-19 11:47:12 +0800

    修复：旋转导致计数归零的问题

--- app/src/main/java/com/example/gitdemo/MainActivity.java
+++ app/src/main/java/com/example/gitdemo/MainActivity.java
@@ -1,12 +1,18 @@
 package com.example.gitdemo;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.inputmethodservice.Keyboard;
 import android.os.Bundle;
+import android.os.PersistableBundle;
 import android.widget.Button;
 import android.widget.TextView;
 
+import java.security.Key;
+
 public class MainActivity extends AppCompatActivity {
+    private static String NUMBER = "number";
     private int number = 0;
     private Button mButtonAdd, mButtonSud;
     private TextView mTextView;
@@ -19,9 +25,18 @@ public class MainActivity extends AppCompatActivity {
         mTextView = findViewById(R.id.textView);
         mButtonSud = findViewById(R.id.buttonSub);
 
+        number = savedInstanceState != null ? savedInstanceState.getInt(NUMBER):0;
+
+
         mTextView.setText(String.valueOf(number));
         mButtonAdd.setOnClickListener(v -> mTextView.setText(String.valueOf(++number)));
         mButtonSud.setOnClickListener(v -> mTextView.setText(String.valueOf(--number)));
 
+    }
+
+    @Override
+    protected void onSaveInstanceState(@NonNull Bundle outState) {
+        super.onSaveInstanceState(outState);
+        outState.putInt(NUMBER,number);
     }
 }
