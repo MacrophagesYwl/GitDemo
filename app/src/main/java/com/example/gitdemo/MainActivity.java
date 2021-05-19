7116e9ed6c755d0399778ee7a3148c891c0f947d
狐白 <1831605011@qq.com>
2021-05-19 12:01:27 +0800

    实现了重置计数功能

--- app/src/main/java/com/example/gitdemo/MainActivity.java
+++ app/src/main/java/com/example/gitdemo/MainActivity.java
@@ -1,15 +1,13 @@
 package com.example.gitdemo;
 
-import androidx.annotation.NonNull;
-import androidx.appcompat.app.AppCompatActivity;
-
-import android.inputmethodservice.Keyboard;
 import android.os.Bundle;
-import android.os.PersistableBundle;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.widget.Button;
 import android.widget.TextView;
 
-import java.security.Key;
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
     private static String NUMBER = "number";
@@ -25,9 +23,6 @@ public class MainActivity extends AppCompatActivity {
         mTextView = findViewById(R.id.textView);
         mButtonSud = findViewById(R.id.buttonSub);
 
-        number = savedInstanceState != null ? savedInstanceState.getInt(NUMBER):0;
-
-
         mTextView.setText(String.valueOf(number));
         mButtonAdd.setOnClickListener(v -> mTextView.setText(String.valueOf(++number)));
         mButtonSud.setOnClickListener(v -> mTextView.setText(String.valueOf(--number)));
@@ -35,8 +30,26 @@ public class MainActivity extends AppCompatActivity {
     }
 
     @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu, menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.zero:
+                mTextView.setText(String.valueOf(number = 0));
+                break;
+            default:
+                break;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+    @Override
     protected void onSaveInstanceState(@NonNull Bundle outState) {
         super.onSaveInstanceState(outState);
-        outState.putInt(NUMBER,number);
+        outState.putInt(NUMBER, number);
     }
 }
