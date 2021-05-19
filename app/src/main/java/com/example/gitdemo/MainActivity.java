61a012223d89d38314ce7e6f16afa58798f40cdf
狐白 <1831605011@qq.com>
2021-05-19 10:03:30 +0800

    实现了buttonAdd的+1功能

--- app/src/main/java/com/example/gitdemo/MainActivity.java
+++ app/src/main/java/com/example/gitdemo/MainActivity.java
@@ -3,12 +3,24 @@ package com.example.gitdemo;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+    private int number = 0;
+    private Button mButtonAdd;
+    private TextView mTextView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        mButtonAdd = findViewById(R.id.buttonAdd);
+        mTextView = findViewById(R.id.textView);
+
+        mTextView.setText(String.valueOf(number));
+        mButtonAdd.setOnClickListener(v ->{
+            mTextView.setText(String.valueOf(++number));
+        });
     }
 }
